19f9a01a7303eae954bf6116fc60a94df3d4655d
Programmert717 <milan.kalashnikov2017@mail.ru>
2025-04-21 12:05:45 +0300

    регистрация+авторизация

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ store-server/store/users/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.urls import reverse
+
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect('/')
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', form: form}
+    return render(request, 'users/profile.html', context)
